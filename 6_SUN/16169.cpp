dd58853d04da0d8f3924f234a02dca9a8dc18522
forreal03 <qwd00363@gmail.com>
2025-09-14 15:39:49 +0900

    UPDATE 0914 FIX

--- 6_SUN/16169.cpp
+++ 6_SUN/16169.cpp
@@ -1,70 +1,61 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <climits>
 #include <queue>
+
 using namespace std;
 
-int main() {
-    cin.tie(0)->sync_with_stdio(0);
+int main(){
 
-    int N; 
-    cin >> N;
-    vector<int> level(N+1), t(N+1);
-    for (int i = 1; i <= N; i++) {
-        cin >> level[i] >> t[i];
-    }
+	cin.tie(0)->sync_with_stdio(0);
 
-    
-    vector<vector<pair<int,int>>> adj(N+1);
-    vector<int> indeg(N+1, 0);
+	int n;
+	cin >> n;
 
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
-            if (level[i] + 1 == level[j]) {
-                int cost = (i - j) * (i - j);
-                adj[i].push_back({j, cost});
-                indeg[j]++;
-            }
-        }
-    }
+	vector<int> level(n, 0);
+	vector<int> time(n, 0);
+	vector<int>indegree(n, 0);
+	vector<long long>dp(n, 0);
+	vector<vector<pair<int ,int>>>adj(n);
 
-    
-    vector<long long> dp(N+1, 0);
-    for (int i = 1; i <= N; i++) {
-        if (indeg[i] == 0) 
-			dp[i] = t[i];
-    }
+	for(int i = 0; i < n; i++){
+		cin >> level[i] >> time[i];
 
-    
-    queue<int> q;
-    for (int i = 1; i <= N; i++) 
-		if (indeg[i] == 0) 
+	}
+
+	for(int i = 0; i < n; i++){
+		dp[i] = time[i];
+	}
+
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			if(level[i] + 1 == level[j]){
+				indegree[j]++;
+				adj[i].emplace_back(j, (i - j) * (i - j));
+			}
+		}
+	}
+
+	queue<int>q;
+
+	for(int i = 0; i < n; i++)
+		if(!indegree[i])
 			q.push(i);
 
-    long long answer = 0;
+	while(!q.empty()){
 
-    while (!q.empty()) {
-        int u = q.front(); 
-		
+		int x = q.front();
 		q.pop();
-        
-		answer = max(answer, dp[u]);
 
-        for (auto [v, w] : adj[u]) {
+		for(auto[nxt, w] : adj[x]){
+			dp[nxt] = max(dp[nxt], dp[x] + w + time[nxt]);
+			if(--indegree[nxt] == 0){
+				q.push(nxt);
+			}
+		}
 
-            dp[v] = max(dp[v], dp[u] + w + t[v]);
 
-            if (--indeg[v] == 0) q.push(v);
-
-        }
-    }
-
-    
+	} cout << *max_element(dp.begin(), dp.end());
 	
-    for (int i = 1; i <= N; i++) 
-		answer = max(answer, dp[i]);
-
-    cout << answer ;
-    return 0;
+	return 0;
 }
